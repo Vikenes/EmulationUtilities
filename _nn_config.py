dbdfecd6c5070a29039e4c2cb2400f3970f8ccde
Vikenes <vetle.vkns@gmail.com>
2023-09-26 10:57:35 +0200

    addded patience and grad_clip_val to config

--- _nn_config.py
+++ _nn_config.py
@@ -41,6 +41,8 @@ class ModelConfig(BaseModel):
 class TrainingConfig(BaseModel):
     gpus: Optional[List[int]]
     max_epochs: int
+    patience: Optional[int] = 20
     stochastic_weight_avg: bool
     swa_start: Optional[int] = 110
+    gradient_clip_val: Optional[float] = None
     default_root_dir: str
